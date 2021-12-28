8887e1a58159aa8352277cbff20d53601e7d6dbc
StormSam <73138760+samunacci27@users.noreply.github.com>
2021-12-28 19:35:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const id_chatWelcome = "";
+//                        
 
+// Membro si unisce al server
+bot.on('guildMemberAdd', member => {
+
+    const EmbedWelcome = new MessageEmbed()
+        .setColor("")// Color 
+        .setTitle("")// Title
+        .setDescription(``)// Description
+        .setThumbnail("")// PNG/GIF image
+        .setFooter("")// final Description
+    
+    const chatbenvenuto = bot.channels.cache.find(channel => channel.id === id_chatWelcome)
+    chatbenvenuto.send(EmbedWelcome)
+});
